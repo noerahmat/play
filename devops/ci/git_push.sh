2188cdb6bd13a9ed1f762b31f168865f80d17d7b
noerahmat <noerahmat08@gmail.com>
2020-03-22 02:28:55 +0000

    2020-03-22-022855'-CICD

--- devops/ci/git_push.sh
+++ devops/ci/git_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-NOW=$(date +"date '+%Y-%m-%d-%H%M%S'")
+NOW=$(date +"%Y-%m-%d-%H%M%S'")-$(git rev-parse --abbrev-ref HEAD)
 #FILE="backup.$NOW.tar.gz"
 #echo "Backing up data to /nas42/backup.$NOW.tar.gz file, please wait..."
 #git add . && git commit -m "$$(date '+%Y-%m-%d-%H%M%S')-$$(git rev-parse --abbrev-ref HEAD)" && git push origin "$$(git rev-parse --abbrev-ref HEAD)"
