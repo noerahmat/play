76294509b0a8e4af5e12953c54425658e901adde
noerahmat <noerahmat08@gmail.com>
2020-03-22 02:17:23 +0000

    1116GIT_COMMIT

--- devops/ci/git_push.sh
+++ devops/ci/git_push.sh
@@ -1,1 +1,3 @@
-git add . && git commit -m "$$(date '+%Y-%m-%d-%H%M%S')-$$(git rev-parse --abbrev-ref HEAD)" && git push origin "$$(git rev-parse --abbrev-ref HEAD)"
+#git add . && git commit -m "$$(date '+%Y-%m-%d-%H%M%S')-$$(git rev-parse --abbrev-ref HEAD)" && git push origin "$$(git rev-parse --abbrev-ref HEAD)"
+GIT_COMMIT=$(date '+%Y-%m-%d-%H%M%S')-$(git rev-parse --abbrev-ref HEAD)
+git add . && git commit -m "$$GIT_COMMIT" && git push origin "$$(git rev-parse --abbrev-ref HEAD)"
