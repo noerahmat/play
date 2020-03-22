59bca6f09f5344dcb0c874d6b7c81cb260966b8c
noerahmat <noerahmat08@gmail.com>
2020-03-22 10:22:25 +0000

    2020-03-22-102225-CICD

--- devops/ci/setup.sh
+++ devops/ci/setup.sh
@@ -1,12 +1,35 @@
-# You have to define the values in {}
+# You have to define thew values in {}
 export DOCKER_USER=mamat08nurahmat
 export DOCKER_PASSWORD=Nurahmat-19
 
-cat <<EOF2 >deploy.env
-APP_NAME=my-super-app
-TAG_VERSION=$(git rev-parse HEAD)
 DOCKER_REPO=mamat08nurahmat
-ENV=sta
+APP_NAME=my-super-app
+APP_PORT=80
+
+GIT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+
+if [ $GIT_BRANCH_NAME = "master" ]
+then
+        TAG_VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
+else
+        ENV=$(grep ENV deploy.env | cut -d '=' -f2)
+        TAG_VERSION=$ENV-$(grep TAG_VERSION deploy.env | cut -d '=' -f2)
+fi
+
+APP_IMAGE=$DOCKER_REPO/$APP_NAME:$TAG_VERSION
+
+
+cat <<EOF1 >.env
+APP_IMAGE=$APP_IMAGE
+APP_PORT=$APP_PORT
+EOF1
+
+
+cat <<EOF2 >deploy.env
+APP_NAME=$APP_NAME
+TAG_VERSION=$(git rev-parse HEAD)
+DOCKER_REPO=$DOCKER_REPO
+ENV=$(git rev-parse --abbrev-ref HEAD)
 EOF2
 
 # optional aws-cli options
@@ -20,3 +43,15 @@ EOF2
 cat <<EOF3 >config.env
 PORT=80
 EOF3
+
+cat <<"EOF4">docker-compose.yaml
+version: '2'
+services:
+  app:
+    image: ${APP_IMAGE}
+    ports:
+      - "${APP_PORT}:80"
+    mem_limit: 128MB
+    mem_reservation: 64MB
+    restart: always
+EOF4
--- devops/ci/test.sh
+++ devops/ci/test.sh
@@ -1,10 +1,26 @@
+DOCKER_REPO=$(grep DOCKER_REPO deploy.env | cut -d '=' -f2)
+APP_NAME=$(grep APP_NAME deploy.env | cut -d '=' -f2)
 #cek git branch
 #if master --> TAG_VERSION= git tag version --> 1.0.0.1
 #if staging     --> TAG_VERSION= sta+commmit_id --> sta-1345abcde
 #if development --> TAG_VERSION= dev+commmit_id --> dev-1345abcde
 #TAG_VERSION = sta-$(grep TAG_VERSION deploy.env | cut -d '=' -f2)
+#ENV=$(grep ENV deploy.env | cut -d '=' -f2)
 
-export TEST_RESULT=$(docker run $(grep DOCKER_REPO deploy.env | cut -d '=' -f2)/$(grep APP_NAME deploy.env | cut -d '=' -f2):$(grep ENV deploy.env | cut -d '=' -f2)-$(grep TAG_VERSION deploy.env | cut -d '=' -f2) /bin/sh -c "[ -e "/usr/share/nginx/html/index.html" ] && echo 'PASS' || echo 'FAIL'")
+#TAG_VERSION=$ENV-$(grep TAG_VERSION deploy.env | cut -d '=' -f2)
+GIT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+
+if [ $GIT_BRANCH_NAME = "master" ]
+then
+	TAG_VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
+else
+	ENV=$(grep ENV deploy.env | cut -d '=' -f2)
+	TAG_VERSION=$ENV-$(grep TAG_VERSION deploy.env | cut -d '=' -f2)
+fi
+
+IMAGE_NAME=$DOCKER_REPO/$APP_NAME:$TAG_VERSION
+
+export TEST_RESULT=$(docker run $IMAGE_NAME /bin/sh -c "[ -e "/usr/share/nginx/html/index.html" ] && echo 'PASS' || echo 'FAIL'")
 
 #if [ "$TEST_RESULT" = 'PASS' ];  echo'TEST IS PASS' && exit 0; else echo'TEST IS FAIL' && exit 1; fi
 
