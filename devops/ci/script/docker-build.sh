fe06dfac679876f09ff6e9d06d6220344376fb9c
noerahmat <noerahmat08@gmail.com>
2020-03-22 13:07:15 +0000

    2020-03-22-130715-CICD

---
+++ devops/ci/script/docker-build.sh
@@ -0,0 +1,1 @@
+docker build --build-arg SVC_PORT=80 -t my-super-app -f ci/infra/nginx/Dockerfile .
--- devops/ci/setup.sh
+++ devops/ci/setup.sh
@@ -5,6 +5,8 @@ export DOCKER_PASSWORD=Nurahmat-19
 DOCKER_REPO=mamat08nurahmat
 APP_NAME=my-super-app
 APP_PORT=80
+SSH_USER=builder001
+SSH_HOST_DEV=ec2-3-1-205-54.ap-southeast-1.compute.amazonaws.com
 
 GIT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 
@@ -18,18 +20,22 @@ fi
 
 APP_IMAGE=$DOCKER_REPO/$APP_NAME:$TAG_VERSION
 
+mkdir -p deploy;
+mkdir -p deploy/$APP_NAME;
 
-cat <<EOF1 >.env
+cat <<EOF1 >ci/.env
 APP_IMAGE=$APP_IMAGE
 APP_PORT=$APP_PORT
 EOF1
 
 
-cat <<EOF2 >deploy.env
+cat <<EOF2 >ci/deploy.env
 APP_NAME=$APP_NAME
 TAG_VERSION=$(git rev-parse HEAD)
 DOCKER_REPO=$DOCKER_REPO
 ENV=$(git rev-parse --abbrev-ref HEAD)
+SSH_USER=$SSH_USER
+SSH_HOST_DEV=$SSH_HOST_DEV
 EOF2
 
 # optional aws-cli options
@@ -40,11 +46,11 @@ EOF2
 
 # Port to run the container 
 # Until here you can define all the individual configurations for your app
-cat <<EOF3 >config.env
-PORT=80
-EOF3
 
-cat <<"EOF4">docker-compose.yaml
+cp ci/.env -p deploy/$APP_NAME/.env;
+cp ci/deploy.env -p deploy/$APP_NAME/deploy.env;
+
+cat <<"EOF4">deploy/$APP_NAME/docker-compose.yaml
 version: '2'
 services:
   app:
